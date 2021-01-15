1b9f1ff943a042a5016105ab861cbfa4b08d5e43
Djamilson <djamilson@gmail.com>
2021-01-14 23:43:51 -0300

    Update

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -8,7 +8,7 @@ export default class SessionsController {
   public async create(req: Request, res: Response): Promise<Response> {
     try {
       const { email, password } = req.body;
-      console.log('logui:::::', req.body);
+
       const authenticateUser = container.resolve(AuthenticateUserService);
 
       const { user, token } = await authenticateUser.execute({

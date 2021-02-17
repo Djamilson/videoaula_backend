885372bbf4be2fa60819db4c8fb7db9de1a920bd
Djamilson <djamilson@gmail.com>
2021-02-17 19:59:22 -0300

    refactoring routes

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -49,7 +49,7 @@ class SendForgotPasswordEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: userExists.person.name,
-          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          link: `${process.env.APP_URL_FRONTEND}/reset_password?token=${token}`,
         },
       },
     });
